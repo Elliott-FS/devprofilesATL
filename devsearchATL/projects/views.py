17b3feff89bc03cb5cdca95f9cfeb92c8d4836b2
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-03-27 22:54:49 -0400

    some business logic examples/notes

--- devsearchATL/projects/views.py
+++ devsearchATL/projects/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id':'1',
+        'title': 'Playbliss',
+        'desc': 'Video game catalog'
+
+    },
+    {
+        'id':'2',
+        'title': 'Playbliss',
+        'desc': 'Video game catalog'
+
+    },
+    {
+        'id':'3',
+        'title': 'Playbliss',
+        'desc': 'Video game catalog'
+
+    }
+    
+]
+
 def projects(request):
     page = 'projects'
     number = 10
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projects': projectsList}
     return render(request, 'devsearchProjects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'devsearchProjects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'devsearchProjects/single-project.html', {'project': projectObj})
 
 
 
--- notes.md
+++ notes.md
@@ -1,2 +1,3 @@
 Rememmber to use python -m pip install django when installing django in ubuntu.
 "python3 manage.py runserver" runs server from my ubuntu terminal.
+From top folder run source djangoenv/bin/activate to turn on venv from my terminal. 
